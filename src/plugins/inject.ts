ab259a7926221fa53aafbccff13946ad030e391d
xhcoding <xiehui@1370195667528290.onaliyun.com>
2023-04-21 18:10:17 +0800

    优化

--- src/plugins/inject.ts
+++ src/plugins/inject.ts
@@ -1,9 +1,9 @@
 // @ts-ignore
 async function getAllCookies() {
   const cookies = await chrome.cookies.getAll(
-    { domain: '.aliyun.com',
-      session: false
-    })
+  { domain: '.aliyun.com',
+    session: false
+  })
   console.log(cookies);
 }
 
